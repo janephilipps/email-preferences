bf645c0b4808de2b2276395f4a9c986c161f0e77
Jane Philipps <jane.philipps@gmail.com>
2017-01-03 21:31:28 -0800

    DRY JS

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,34 +1,17 @@
 $(document).ready(function() {
 
     var doNotEmail = $('#user_do_not_email');
-    var marketing = $('#user_marketing');
-    var articles = $('#user_articles');
-    var digest = $('#user_digest');
+    var email = $('.email');
 
     doNotEmail.change(function(){
         if (this.checked) {
-            marketing.attr('checked', false);
-            articles.attr('checked', false);
-            digest.attr('checked', false);
+            email.attr('checked', false);
         }
     });
 
-    marketing.change(function(){
+    email.change(function(){
         if (this.checked) {
             doNotEmail.attr('checked', false);
         }
     });
-
-    articles.change(function(){
-        if (this.checked) {
-            doNotEmail.attr('checked', false);
-        }
-    });
-
-    digest.change(function(){
-        if (this.checked) {
-            doNotEmail.attr('checked', false);
-        }
-    });
-
 });
